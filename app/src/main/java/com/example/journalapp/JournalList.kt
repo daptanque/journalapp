80e81a5f46933209f7fe3c2d7f2915f62415ca96
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-04-03 09:13:06 +0100

    JournalList Activity setup

--- app/src/main/java/com/example/journalapp/JournalList.kt
+++ app/src/main/java/com/example/journalapp/JournalList.kt
@@ -2,15 +2,52 @@ package com.example.journalapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.journalapp.databinding.ActivityJournalListBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
 
 class JournalList : AppCompatActivity() {
 
     lateinit var binding: ActivityJournalListBinding
 
+    //Firebase References
+    lateinit var firebaseAuth : FirebaseAuth
+    lateinit var firebaseUser: FirebaseUser
+    var db = FirebaseFirestore.getInstance()
+    lateinit var storageReference : StorageReference
+    var collectionReference : CollectionReference = db.collection("Journal")
+
+    lateinit var journalList : List<Journal>
+    lateinit var journalRecyclerAdapter: JournalRecyclerAdapter
+
+    lateinit var noPostsTextView: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_journal_list)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_journal_list)
+
+        //Firebase Auth
+        firebaseAuth = Firebase.auth
+        firebaseUser = firebaseAuth.currentUser!!
+
+        //RecyclerView
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager =
+                LinearLayoutManager(this)
+
+        //Posts arrayList
+        journalList = arrayListOf<Journal>()
 
 
     }
